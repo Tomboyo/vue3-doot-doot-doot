6918997e495899a995e9578af947536f96b1231c
Tom Simmons (Tomboyo) <tomasimmons@gmail.com>
2023-03-01 20:14:59 -0500

    books selector

---
+++ frontend/src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { computed, ref, reactive } from "vue"
+const books = [
+  {
+    author: 'Bloch',
+    title: 'Effective Java'
+  },
+  {
+    author: 'Evil Hat',
+    title: 'Fate Core System'
+  }];
+
+const selectedAuthor = ref(false);
+const currentBook = computed(() => books.find(book => book.author === selectedAuthor.value))
+</script>
+
+<template>
+  <header>
+    <h1>Books</h1>
+  </header>
+
+  <main>
+    <select v-model="selectedAuthor">
+      <option disabled>Please select an author</option>
+      <option v-for="book in books" :value="book.author">{{ book.author }}</option>
+    </select>
+    <div v-if="selectedAuthor">{{ currentBook.title }}, by {{ currentBook.author }}</div>
+  </main>
+</template>
+
+<style scoped>
+</style>
